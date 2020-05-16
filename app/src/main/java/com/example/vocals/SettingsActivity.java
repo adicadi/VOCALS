8e5306d1fb7e6ee84c41344a5b9fd238b2b88ca7
Aditya chaudhary <50004633+ADICADI@users.noreply.github.com>
2020-05-16 23:31:45 +0530

    Status Activity connected with settings activity,and change status and change image buttons are now working.

--- app/src/main/java/com/example/vocals/SettingsActivity.java
+++ app/src/main/java/com/example/vocals/SettingsActivity.java
@@ -1,14 +1,22 @@
 package com.example.vocals;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,8 +24,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.w3c.dom.Text;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,6 +46,13 @@ public class SettingsActivity extends AppCompatActivity {
     private CircleImageView mDisplayImage;
     private TextView mName;
     private TextView mStatus;
+    private Button mStatusbtn;
+    private Button mImagebtn;
+
+    private static final int GALLERY_PICK = 1;
+
+    //firebase storage
+    private StorageReference mProfileimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +72,11 @@ public class SettingsActivity extends AppCompatActivity {
         mName=(TextView) findViewById(R.id.settings_display_name);
         mStatus=(TextView) findViewById(R.id.settings_status);
 
+        mStatusbtn=(Button) findViewById(R.id.settings_status_btn);
+        mImagebtn=(Button) findViewById(R.id.settings_image_btn);
 
+        //firebase
+        mProfileimage = FirebaseStorage.getInstance().getReference();
         mCurrentUser= FirebaseAuth.getInstance().getCurrentUser();
         String current_uid=mCurrentUser.getUid();
 
@@ -57,6 +84,7 @@ public class SettingsActivity extends AppCompatActivity {
         mUserDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
                 String name =dataSnapshot.child("name").getValue().toString();
                 String image =dataSnapshot.child("image").getValue().toString();
                 String status =dataSnapshot.child("status").getValue().toString();
@@ -65,7 +93,7 @@ public class SettingsActivity extends AppCompatActivity {
                 mName.setText(name);
                 mStatus.setText(status);
 
-
+                Picasso.get().load(image).into(mDisplayImage);
 
             }
 
@@ -74,5 +102,77 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+        mStatusbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent status_intent = new Intent(SettingsActivity.this,StatusActivity.class);
+                startActivity(status_intent);
+            }
+        });
+        mImagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(galleryIntent, "Select image"), GALLERY_PICK);
+
+                /*
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);
+                */
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+            assert data != null;
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+            //Toast.makeText(SettingsActivity.this,imageUri, Toast.LENGTH_SHORT).show();
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                String current_user_id = mCurrentUser.getUid();
+
+                final StorageReference filepath = mProfileimage.child("profile_images").child(current_user_id +".jpg");
+
+                filepath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot){
+                        filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String download_url = uri.toString();
+                                mUserDatabase.child("image").setValue(download_url).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(SettingsActivity.this,"Success Uploading",Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/vocals/StatusActivity.java
+++ app/src/main/java/com/example/vocals/StatusActivity.java
@@ -1,21 +1,57 @@
 package com.example.vocals;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StatusActivity extends AppCompatActivity {
+
+    private TextInputEditText mStatus;
+
+
+    //firebase
+    private DatabaseReference mStatusDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
+        //firebase
+        FirebaseUser mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid= mCurrentUser.getUid();
+
+        mStatusDatabase= FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
         //Toolbar set
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.status_appBar);
+        Toolbar mToolbar = findViewById(R.id.status_appBar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Status");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String status_value = getIntent().getStringExtra("status_value");
+
+        mStatus = findViewById(R.id.status_input);
+        Button mSavebtn = findViewById(R.id.status_save_btn);
+
+
+        mSavebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String status=mStatus.getEditableText().toString();
+                mStatusDatabase.child("status").setValue(status);
+
+            }
+        });
     }
 }
